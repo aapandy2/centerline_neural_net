c2db3e2a02a4d416753742529040827bec6d34f3
Alex Pandya <aapandy2@illinois.edu>
2018-07-06 15:23:29 -0400

    best working version of loc_net.py

--- loc_net.py
+++ loc_net.py
@@ -1,4 +1,3 @@
-#from __future__ import print_function
 import keras
 import scipy.io
 from keras.layers import Dense, Flatten, GaussianNoise
@@ -25,11 +24,10 @@ frames_directory_test = '/scratch/network/apandya/cam1_frames_resized3/'
 frames_directory3 = '/scratch/network/apandya/cam1_frames_resized_test/'
 frames_directory4 = '/scratch/network/apandya/cam1_frames_resized4/'
 frames_directory5 = '/scratch/network/apandya/cam1_frames_resized5/'
+frames_directory6 = '/scratch/network/apandya/cam1_frames_resized6/'
 
-#num_classes = 10
-
-num_x_bins = 6
-num_y_bins = 6
+num_x_bins = 15
+num_y_bins = 15
 
 def generate_bin_array(centerline_array):
         true_coords_array = centerline_array.astype(int)
@@ -89,16 +87,14 @@ def load_images(frames_directory, num_images_to_load, first_image_index, step):
                 
 		#do some elementary image enhancement
                 img_loaded = np.array(img_loaded) / np.mean(img_loaded) * 0.25
-                img_loaded[img_loaded < 0.2] = 0.
+                img_loaded[img_loaded < 0.1] = 0.
                 img_loaded[img_loaded > 1.] = 1.
 
-		#flatten arrays
-#		coords_flattened = np.array(coords_np[1::(100/(num_classes/2))]).flatten() / 300.
+		#flatten image array
 		img_flattened    = img_loaded.flatten()
 
                 #reshape flattened coords array to be 1D columns (net input format)
-#                coords_flattened = np.reshape(coords_flattened, (1, num_classes))
-		bin_array_flattened = np.reshape(bin_array, (1, num_x_bins*num_y_bins)) / 100. #TODO:remove this
+		bin_array_flattened = np.reshape(bin_array, (1, num_x_bins*num_y_bins)) / 100.
 
                 image_data.append(img_flattened)
                 coords_data.append(bin_array_flattened[0])
@@ -109,7 +105,7 @@ def load_images(frames_directory, num_images_to_load, first_image_index, step):
 
 #load num_images_to_load, stepping through by step, starting at 
 #first_image_index
-step = 100
+step = 20
 first_image_index = 0
 
 #training parameters
@@ -134,25 +130,27 @@ def combine_training_data(num_training_sets):
 
 	total_images = np.sum(size_array[0:num_training_sets]) / step
 	print 'total_images', total_images
-#
+
         combined_imgs = np.reshape(combined_imgs, (total_images, img_x, img_y))
         combined_centerlines = np.reshape(combined_centerlines, (total_images, num_x_bins*num_y_bins))
 
 	combined_array = [combined_imgs, combined_centerlines]
 	return combined_array
 
-#combined_imgs, combined_centerlines = combine_training_data(1)
-#for i in range(10):
-#	bin_img = Image.fromarray(combined_centerlines[i].reshape((num_x_bins, num_y_bins)))
-#	print combined_centerlines[i].reshape((num_x_bins, num_y_bins))
-#	bin_img = bin_img.resize((300,300))
-#	
-#	pl.contourf(bin_img, alpha=0.2, cmap='jet')
-#	pl.imshow(combined_imgs[i], cmap='gray')
-#	pl.show()
+def display_training_data():
+	combined_imgs, combined_centerlines = combine_training_data(1)
+	for i in range(10):
+		bin_img = Image.fromarray(combined_centerlines[i].reshape((num_x_bins, num_y_bins)))
+		print combined_centerlines[i].reshape((num_x_bins, num_y_bins))
+		bin_img = bin_img.resize((300,300))
+		
+		pl.contourf(bin_img, alpha=0.2, cmap='jet')
+		pl.imshow(combined_imgs[i], cmap='gray')
+		pl.show()
+	return 0.
 
 if(mode == TRAIN_MODEL):
-	combined_imgs, combined_centerlines = combine_training_data(3)
+	combined_imgs, combined_centerlines = combine_training_data(4)
 
 	x_train = combined_imgs
 	y_train = combined_centerlines
@@ -164,11 +162,9 @@ if(mode == TRAIN_MODEL):
 	
 	x_train = x_train.reshape(x_train.shape[0], img_x, img_y, 1)
 	input_shape = (img_x, img_y, 1)	
-#	x_train = x_train.reshape(x_train.shape[0], img_x*img_y, 1)
-#	input_shape = (img_x*img_y, 1)
 
 	model = Sequential()
-	model.add(Conv2D(64, kernel_size=(3, 3), strides=(2, 2),
+	model.add(Conv2D(4, kernel_size=(3, 3), strides=(2, 2),
 	                 activation='relu',
 	                 input_shape=input_shape,
 			 kernel_initializer='random_uniform'))
@@ -179,21 +175,10 @@ if(mode == TRAIN_MODEL):
 	model.add(Conv2D(64, kernel_size=(3, 3), strides=(2, 2),
                          activation='relu'))
         model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
-#	model.add(Conv2D(64, kernel_size=(3, 3), strides=(2, 2),
-#                         activation='relu'))
-#        model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
 	model.add(Flatten())
-#	model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
-#	model.add(Conv2D(64, kernel_size=(3, 3), activation='relu'))
-#	model.add(MaxPooling2D(pool_size=(2, 2)))
-#	model.add(Conv2D(64, kernel_size=(3, 3), activation='relu'))
-#        model.add(MaxPooling2D(pool_size=(2, 2)))
-#	model.add(Flatten())
 	model.add(Dense(num_x_bins*num_y_bins, activation='sigmoid'))
 	
-#	model.compile(loss=keras.losses.binary_crossentropy,
 	model.compile(loss=keras.losses.mean_squared_error,
-#	model.compile(loss=keras.losses.categorical_crossentropy,
 	              optimizer=keras.optimizers.Adam())
 #		      optimizer=keras.optimizers.SGD(lr=0.30))
 	
@@ -211,15 +196,15 @@ if(mode == TRAIN_MODEL):
 	          batch_size=batch_size,
 	          epochs=epochs,
 	          verbose=1,
-	          callbacks=[history])
+		  callbacks=[history],
+		  validation_split=0.1)
 	
-	# Creates a HDF5 file 'my_model1.h5'
-#	model.save('my_model_retrain.h5')
+	# Creates a HDF5 file [model_name].h5
 	model.save(model_name)
 
 
 if(mode == TRAIN_LOADED_MODEL):
-	combined_imgs, combined_centerlines = combine_training_data(5)
+	combined_imgs, combined_centerlines = combine_training_data(4)
 
 	x_train = combined_imgs
         y_train = combined_centerlines
@@ -238,7 +223,6 @@ if(mode == TRAIN_LOADED_MODEL):
                       optimizer=keras.optimizers.Adam())
         #             optimizer=keras.optimizers.SGD(lr=0.30))
 
-
         class AccuracyHistory(keras.callbacks.Callback):
             def on_train_begin(self, logs={}):
                 self.acc = []
@@ -254,7 +238,7 @@ if(mode == TRAIN_LOADED_MODEL):
                   verbose=1,
                   callbacks=[history])
 
-        # Creates a HDF5 file 'my_model1.h5'
+        # Creates a HDF5 file [model_name].h5
         model.save(model_name)
 
 if(mode == LOAD_MODEL):
@@ -264,13 +248,10 @@ if(mode == LOAD_MODEL):
 	num_test_images = 15000
 	test_step       = 100
 
-	x_test, y_test = load_images(frames_directory4, num_test_images, 0, test_step)
+	x_test, y_test = load_images(frames_directory6, num_test_images, 0, test_step)
 
 	x_test = x_test.astype('float32')
 	x_test  = x_test.reshape(x_test.shape[0], img_x, img_y, 1)
-#	x_test  = x_test.reshape(x_test.shape[0], img_x*img_y, 1)
-
-#	print y_test[0].reshape((5,5))
 
 	num_test_sample_images = 10
 	
@@ -283,19 +264,11 @@ if(mode == LOAD_MODEL):
 		#choose random test image from test_data
 		test_image = np.random.randint(low=0, high= int(num_test_images/test_step) )
 		
-		#retrieve image from array and convert it to color to draw colored centerline
-#		img = Image.fromarray(np.reshape(x_test[test_image], (300, 300)) * 255).convert('RGBA')
-#		draw = ImageDraw.Draw(img)
+		#retrieve image from array and convert it to correct format for display
 	        img_reshaped = np.reshape(x_test[test_image], (300, 300)) * 255
 	
-		
-		#generate net centerline and retrieve "true" centerline from array 
-		#and convert them to tuples of (x, y) tuples for draw.line() below
-#		net_coords_tuple = tuple(map(tuple, np.reshape(predictions[test_image], (num_classes/2, 2))))
-#		net_coords_tuple = tuple(map(tuple, np.reshape(np.array(model.predict(x_test))[test_image] * 300., (num_classes/2, 2))))
-#		true_coords_tuple = tuple(map(tuple, np.reshape(np.array(y_test[test_image])*300., (200/2, 2))))
-
-		true_ans = y_test[test_image].reshape(num_x_bins, num_y_bins) #TODO: reverse x and y?
+		#generate net's prediction and the 'true' answer computed from centerline
+		true_ans = y_test[test_image].reshape(num_x_bins, num_y_bins)
 		net_ans  = predictions[test_image].reshape(num_x_bins, num_y_bins)
 
 		print 'net:', net_ans, 'true:', true_ans
@@ -303,26 +276,9 @@ if(mode == LOAD_MODEL):
 		bin_img = Image.fromarray(net_ans)
 	        bin_img = bin_img.resize((300,300))
 
-	        pl.contourf(bin_img, alpha=0.2, cmap='Blues')
+	        pl.contourf(bin_img, alpha=0.25, cmap='gnuplot')
 	        pl.imshow(img_reshaped, cmap='gray')
 	        pl.show()
-
-#		print true_ans
-#		print net_ans
-
-#		true_coords_array = np.array(true_coords_tuple)
-
-#		print generate_bin_array(true_coords_array)
-#		print predictions[test_image]
-		
-		#draw net centerline and "true" centerline on test image and show image
-#		if(num_classes > 2):
-#			draw.line(net_coords_tuple, fill=(0, 0, 255), width=2)
-#			draw.line(true_coords_tuple, fill=(255, 0, 0), width=2)
-#		#	img.show()
-#			img.save('output_' + str(k) + '.png')
-#		else:
-#			print 'right ans:', true_coords_tuple, 'net ans:', net_coords_tuple
 
 #clear the data tensorflow saves about old models
 import tensorflow as tf
